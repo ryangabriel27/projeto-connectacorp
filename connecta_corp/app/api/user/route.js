b744bc43fb583933bab0cdafe6886987d5ebab50
ryangabriel27 <ryanjulio56@gmail.com>
2024-09-12 13:24:05 -0300

    readme atualizado

---
+++ connecta_corp/app/api/user/route.js
@@ -0,0 +1,8 @@
+import User from "@/models/User";
+import connectMongo from "@/utils/dbConnect";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import jwt from "jsonwebtoken";
+
+// Alterar informações do usuário
+
